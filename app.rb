d42fda5bff6e945347504ac96879de010873e9e5
elicoon <elicoon@gmail.com>
2020-03-05 00:58:00 +0000

    class updates

--- app.rb
+++ app.rb
@@ -46,6 +46,16 @@ end
 get "/events/:id/rsvps/create" do
     puts "params: #{params}"
 
+    #first, fid the event that rsvping for
+    @event = events_table.where(id: params[:id]).to_a[0]
+    #next, want to insert a row in the rsvps table with the rsvp form data
+    rsvps_table.insert(
+        event_id: @event[:id],
+        name: params["name"],
+        email: params["email"],
+        comments: params["comments"],
+        going: params["going"]
+    )
     view "create_rsvp"
 end
 
